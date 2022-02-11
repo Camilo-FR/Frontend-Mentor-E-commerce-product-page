e42a4ca1b7dbb1c0cd42e8735bf328fdf820c477
Camille <camille.vacher@live.fr>
2022-02-11 18:51:26 +0100

    ajout js sur panier + lightbox en cours

--- js/script.js
+++ js/script.js
@@ -8,6 +8,17 @@ const cartModal = document.querySelector(".cart-modal");
 const addProduct = document.querySelector(".add-product");
 const cartProduct = document.querySelector(".cart-product");
 const cartChoice = document.querySelector(".cart-choice");
+const topProductText = document.querySelector(".top-product-text");
+const topProductDelete = document.querySelector(".top-product-delete");
+const result = document.querySelector(".result-text");
+const slider = document.querySelector(".slider");
+const overlayOn = document.querySelector(".bg-opacity");
+const closeSlider = document.querySelector(".close-slider");
+const nextSlider = document.querySelector(".next-slider");
+const previousSlider = document.querySelector(".previous-slider");
+const thumbnailSlider = document.querySelector(".thumbnail-slider");
+const thumbnailSliderImg = document.querySelectorAll(".thumbnail-slider-img");
+const bigProdSlider = document.querySelectorAll(".big-prod-slider");
 
 //Partie Thumbnail
 
@@ -42,6 +53,8 @@ less.addEventListener("click", () => {
   }
 });
 
+// Partie sur le panier
+
 panier.addEventListener("click", () => {
   cartModal.classList.toggle("show");
 });
@@ -50,4 +63,77 @@ addProduct.addEventListener("click", () => {
   // On enlève 'cart is empty.' et on affiche le/les produit(s) concerné(s)
   cartProduct.classList.remove("show");
   cartChoice.classList.add("show");
+
+  topProductText.textContent =
+    "Fall Limited Edition Sneakers $125.00 x " + quantity.textContent;
+
+  result.textContent = " $" + 125 * quantity.textContent + ".00";
 });
+
+// Pour delete le panier
+
+topProductDelete.addEventListener("click", () => {
+  cartChoice.classList.remove("show");
+  cartProduct.classList.add("show");
+});
+
+// Slider apparition
+
+bigProd.forEach((bigImg) => {
+  bigImg.addEventListener("click", () => {
+    slider.classList.add("show");
+    overlayOn.classList.add("show");
+    thumbnailSlider.classList.add("show");
+  });
+});
+
+closeSlider.addEventListener("click", () => {
+  slider.classList.remove("show");
+  overlayOn.classList.remove("show");
+  thumbnailSlider.classList.remove("show");
+});
+
+// thumbnail slider
+
+thumbnailSliderImg.forEach((img) => {
+  img.addEventListener("click", () => {
+    thumbnailSliderImg.forEach((pic) => {
+      pic.classList.remove("thumbnail-border");
+    });
+
+    img.classList.add("thumbnail-border");
+
+    // console.log(img.dataset.slider);
+
+    const bigProdShowSlider = document.querySelector(
+      ".bigProdSlider" + img.dataset.slider
+    );
+
+    bigProdSlider.forEach((pic) => {
+      pic.classList.remove("show");
+    });
+
+    console.log(bigProdShowSlider);
+    bigProdShowSlider.classList.add("show");
+  });
+});
+
+// thumbnailSliderImg.forEach((img) => {
+//   img.addEventListener("click", () => {
+//     thumbnailSliderImg.forEach((img) =>
+//       img.classList.remove("thumbnail-border")
+//     );
+//     img.classList.add("thumbnail-border");
+
+//     console.log(img.dataset.slider);
+//     const bigProdShowSlider = document.querySelector(
+//       ".bigProdSlider" + img.dataset.slider
+//     );
+
+//     bigProdSlider.forEach((pic) => {
+//       pic.classList.remove("show");
+//     });
+
+//     bigProdShowSlider.classList.add("show");
+//   });
+// });
